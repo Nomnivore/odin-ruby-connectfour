cbb7ebccfa82e656322c5cb196bee099bd62d4aa
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2021-06-02 16:22:30 -0400

    method #display_rules

--- lib/display.rb
+++ lib/display.rb
@@ -5,7 +5,7 @@ require 'colorize'
 module ConnectFourGame
   # Extra methods for displaying stuff to the console
   module Display
-    def title
+    def display_title
       figlet = <<~'FIG'
           ____                            _     _____
          / ___|___  _ __  _ __   ___  ___| |_  |  ___|__  _   _ _ __
@@ -15,6 +15,20 @@ module ConnectFourGame
       FIG
       print figlet.blue
       horiz_line
+    end
+
+    def display_rules
+      rules = <<~'RUL'
+        Two players will take turns dropping a token into one of #{Logic::SIZE_ROWS} columns.
+        If a player gets 4 of their tokens to appear consecutively and either
+        horizontally, vertically, or diagonally, they win.
+        If the board fills up and nobody has won, the game is over.
+        This game is currently Human vs Human.
+
+      RUL
+      print rules
+      horiz_line
+      puts 'Are you ready to play?'
     end
 
     def horiz_line
